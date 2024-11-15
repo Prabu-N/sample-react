134a004da9e31f6e65355595702cf7ff1de6dae0
Prabu-N <70022618+Prabu-N@users.noreply.github.com>
2024-11-15 14:59:24 +0530

    check

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route
           path="/"
           element={
-            <Suspense fallback={<></>}>
+            <Suspense fallback={<div></div>}>
               <Home />
             </Suspense>
           }
